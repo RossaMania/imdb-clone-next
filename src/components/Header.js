6f6c8cbaf5dad7072cd491508437c9c89191d6ce
Ross Clettenberg <raclettenberg@gmail.com>
2023-08-20 15:59:09 -0500

    Copied MenuItem component in Header component. Passed ABOUT title prop, /about address prop, and a different icon from react-icons to MenuItem.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,19 @@
 import React from "react"
 import MenuItem from "./MenuItem"
 import { AiFillHome } from "react-icons/ai";
+import { BsFillInfoCircleFill } from "react-icons/bs";
 
 const Header = () => {
   return (
     <div>
       <div>
-      {/* Menu */}
-      <MenuItem title="HOME" address="/" Icon={AiFillHome} />
+        {/* Menu */}
+        <MenuItem title="HOME" address="/" Icon={AiFillHome} />
+        <MenuItem title="ABOUT" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
       <div>{/* Logo */}</div>
     </div>
-  )
+  );
 }
 
 export default Header;
