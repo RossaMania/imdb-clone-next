e407fb848823703a0bf6e2aecbac96498ec27200
Ross Clettenberg <raclettenberg@gmail.com>
2023-08-21 19:41:19 -0500

    Create about folder with page.js in app folder.

---
+++ src/app/about/page.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const About = () => {
+  return (
+    <div>
+    <h1 className="text-2xl font-medium text-amber-600">
+    About
+    </h1>
+    </div>
+  )
+}
+
+export default About
