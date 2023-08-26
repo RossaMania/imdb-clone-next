eb984928e041e6874c6eccaa41120c4a3a088eba
Ross Clettenberg <raclettenberg@gmail.com>
2023-08-26 14:46:28 -0500

    Import Link into Card. Pass image prop to Card. Make Link point to dynamic movie page.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Card = ({ result }) => {
   return (
-    <div className="cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200">{result.id}</div>
-  )
-}
+    <div className="cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200">
+      <Link href={`/movie/${result.id}`}>
+        <Image
+          src={`https://image.tmdb.org/t/p/original/${
+            result.backdrop_path || result.poster_path
+          }`}
+          width={500}
+          height={300}
+          alt="Movie Poster"
+        />
+      </Link>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
