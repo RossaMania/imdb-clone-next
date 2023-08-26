4a247e8f44346561be5ed9d4c8a0e866ab845b2a
Ross Clettenberg <raclettenberg@gmail.com>
2023-08-25 21:13:36 -0500

    Add regular HTML tags in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html async="" lang="en">
+    <html>
       <body>
         <Providers>
           {/* Header */}
