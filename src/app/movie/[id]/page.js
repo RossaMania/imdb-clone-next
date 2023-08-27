89e8fdb0e2aa931a4af76633b20b562d8df571ea
Ross Clettenberg <raclettenberg@gmail.com>
2023-08-27 16:06:02 -0500

    Pass the image and overview. Style image, title, and overview as Image, h2, and p elements.

--- src/app/movie/[id]/page.js
+++ src/app/movie/[id]/page.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const getMovie = async (movieId) => {
@@ -16,8 +17,28 @@ const MoviePage = async ({ params }) => {
   const movie = await getMovie(movieId);
 
   return (
-    <div>
-      <h1>{movie.title}</h1>
+    <div className="w-full">
+      <div className="p-4 md:pt-8 flex flex-col md:flex-row items-center content-center max-w-6xl mx-auto md:space-x-6">
+        <Image
+          src={`https://image.tmdb.org/t/p/original/${
+            movie.backdrop_path || movie.poster_path
+          }`}
+          width={500}
+          height={300}
+          alt={movie.title}
+          className="rounded-lg"
+          style={{ maxWidth: "100%", height: "100%" }}
+          placeholder="blur"
+          blurDataURL="/spinner.svg"
+        />
+        <div className="p-2">
+          <h2 className="text-lg mb-3 font-bold">{movie.title || movie.name}</h2>
+          <p className="text-lg mb-3">
+            <span className="font-semibold mr-1">Overview:</span>
+            {movie.overview}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
