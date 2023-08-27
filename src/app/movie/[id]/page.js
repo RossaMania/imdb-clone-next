3d8cbb366d471205f3a439241c88c41300ffab93
Ross Clettenberg <raclettenberg@gmail.com>
2023-08-27 15:29:44 -0500

    Fetch movie data and display movie title in page.js by passing params.

--- src/app/movie/[id]/page.js
+++ src/app/movie/[id]/page.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
-const MoviePage = () => {
+const getMovie = async (movieId) => {
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.API_KEY}`
+  );
+
+  return await res.json();
+
+};
+
+const MoviePage = async ({ params }) => {
+
+  const movieId = params.id;
+  const movie = await getMovie(movieId);
+
   return (
-    <div>MoviePage</div>
-  )
-}
+    <div>
+      <h1>{movie.title}</h1>
+    </div>
+  );
+};
 
-export default MoviePage
+export default MoviePage;
