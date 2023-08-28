2e97f998ccb1b8f58ad9767fee574e0054ea1f33
Ross Clettenberg <raclettenberg@gmail.com>
2023-08-27 20:09:06 -0500

    Create handleSubmit func and import useRouter to console log search input, and navigate user to search query.

--- src/components/Searchbox.js
+++ src/components/Searchbox.js
@@ -1,14 +1,28 @@
 "use client"
 
+import { useRouter } from "next/navigation";
 import React, { useState } from "react"
 
 const Searchbox = () => {
 
   const [search, setSearch] = useState("");
+
+  const router = useRouter();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(search);
+
+    if (!search) return;
+
+    router.push(`/search/${search}`);
+
+  }
+
   return (
-    <form className="flex max-w-6xl mx-auto justify-between items-center px-5">
+    <form onSubmit={handleSubmit} className="flex max-w-6xl mx-auto justify-between items-center px-5">
       <input value={search} onChange={(e) => setSearch(e.target.value)} type="text" placeholder="Search keywords..." className="w-full h-14 rounded-sm placeholder-gray-500 outline-none bg-transparent flex-1"/>
-      <button type="submit" className="text-amber-500 disabled:text-gray-400">Search</button>
+      <button disabled={!search} type="submit" className="text-amber-500 disabled:text-gray-400">Search</button>
     </form>
   )
 }
