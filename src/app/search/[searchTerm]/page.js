4089b4f0ba4a2efb20692193662d14f45a033881
Ross Clettenberg <raclettenberg@gmail.com>
2023-08-27 20:36:23 -0500

    Create if throw for fetch errors. Create data const for await res.json, and results const for data.results.

--- src/app/search/[searchTerm]/page.js
+++ src/app/search/[searchTerm]/page.js
@@ -5,6 +5,15 @@ const SearchPage = async (params) => {
     `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${params.searchTerm}&language=en-US&include_adult=false`
   );
 
+  if(!res.ok) {
+    throw new Error("Error fetching data!");
+  }
+
+  const data = await res.json();
+
+  const results = data.results;
+
+
   return <div>SearchPage</div>;
 };
 
