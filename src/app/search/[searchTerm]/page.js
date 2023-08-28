d5f8e531be6d3231de7e3235d1d03beba88ed588
Ross Clettenberg <raclettenberg@gmail.com>
2023-08-27 20:27:08 -0500

    Change SearchPage to async arrow func component. Create res const to await fetch for params.searchTerm.

--- src/app/search/[searchTerm]/page.js
+++ src/app/search/[searchTerm]/page.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
-function SearchPage() {
-  return (
-    <div>SearchPage</div>
-  )
-}
+const SearchPage = async (params) => {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${params.searchTerm}&language=en-US&include_adult=false`
+  );
 
-export default SearchPage
+  return <div>SearchPage</div>;
+};
+
+export default SearchPage;
