e702cd32c71992074e6dd88d18cc63785df053aa
Ross Clettenberg <raclettenberg@gmail.com>
2023-08-27 21:35:42 -0500

    Edit About page text.

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -14,23 +14,17 @@ const About = () => {
         Our website is designed to provide you with a comprehensive database of
         movies from all around the world, along with the latest news, reviews,
         and trailers. Our movie database uses the API provided by The Movie
-        Database (TMDB) ensuring that you have access to the latest and
-        greatest in the world of cinema. You can search for movies by title,
-        director, actor, genre, or release date, making it easy to find the
-        perfect movie for any occasion!
+        Database (TMDB) ensuring that you have access to the latest and greatest
+        in the world of cinema. You can search for movies by title, director,
+        actor, genre, or release date, making it easy to find the perfect movie
+        for any occasion!
       </p>
       <p>
-        In addition to our extensive movie database provided by The Movie
-        Database (TMDB), we also offer a platform for movie lovers to connect
-        and share their thoughts on the latest releases. Our community section
-        includes a forum where you can discuss your favorite films with
-        like-minded individuals and read reviews and ratings from other users.
-        We also have a section dedicated to movie news and trailers, keeping you
-        updated with the latest happenings in the world of cinema. Thank you for
-        visiting our website and we hope you enjoy your time browsing through
-        our movie database. If you have any feedback or suggestions, please feel
-        free to contact us. We are always looking for ways to improve and
-        enhance the use experience on our website. Happy browsing!
+        Thank you for visiting our website and we hope you enjoy your time
+        browsing through our movie database provided by The Movie Database
+        (TMDB) API. If you have any feedback or suggestions, please feel free to
+        contact us. We are always looking for ways to improve and enhance the
+        use experience on our website. Happy browsing!
       </p>
     </div>
   );
