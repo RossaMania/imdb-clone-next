3b1b07ef7f519e8aaee9a60882b840fc7a7ddef7
Ross Clettenberg <raclettenberg@gmail.com>
2023-08-27 19:48:20 -0500

    Add state for search. Add in-line function and value to capture input.

--- src/components/Searchbox.js
+++ src/components/Searchbox.js
@@ -1,11 +1,13 @@
 "use client"
 
-import React from "react"
+import React, { useState } from "react"
 
 const Searchbox = () => {
+
+  const [search, setSearch] = useState("");
   return (
     <form className="flex max-w-6xl mx-auto justify-between items-center px-5">
-      <input type="text" placeholder="Search keywords..." className="w-full h-14 rounded-sm placeholder-gray-500 outline-none bg-transparent flex-1"/>
+      <input value={search} onChange={(e) => setSearch(e.target.value)} type="text" placeholder="Search keywords..." className="w-full h-14 rounded-sm placeholder-gray-500 outline-none bg-transparent flex-1"/>
       <button type="submit" className="text-amber-500 disabled:text-gray-400">Search</button>
     </form>
   )
